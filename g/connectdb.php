e989c04177e86983ffc7eab81e70a4dccf9d5be4
siriwatphudcha-got <siriwatphudcha@gmail.com>
2026-01-28 16:21:14 +0700

    9

--- g/connectdb.php
+++ g/connectdb.php
@@ -1,7 +1,7 @@
 <?php
 		$host = "localhost";
 		$user = "root";
-		$pwd = "";
+		$pwd = "Golf@2004";
 		$db = "4019db";
 		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 		mysqli_query($conn, "SET NAMES utf8");
