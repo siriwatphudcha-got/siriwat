79eb800fd362eb3c298efc8a8b8e095e8d67db27
siriwatphudcha-got <siriwatphudcha@gmail.com>
2026-02-18 13:41:57 +0700

    hftsef

---
+++ j/1.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>สิริวัฒน์ พุดชา (ก็อต)</title>
+</head>
+
+<body>
+<h1>งาน j----- สิริวัฒน์ พุดชา (ก็อต)</h1>
+
+<img src="1.PNG" alt="รูปของสิริวัฒน์ พุดชา" width="250">
+
+</body>
+</html>
